45ced1973e8b1ed46236f0c7720f0dd9447add80
eggeggss <eggeggss@gmail.com>
2019-04-14 09:14:41 +0800

    modify

--- tools/util.py
+++ tools/util.py
@@ -1,5 +1,14 @@
 import matplotlib.pyplot as plt
 
+def show_train_history(train_history,train,validation):
+    plt.plot(train_history.history[train])
+    plt.plot(train_history.history[validation])
+    plt.title('Train History')
+    plt.ylabel(train)
+    plt.xlabel('Epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+
 def plot_images_labels_prediction(images,
                                   labels,
                                   prediction,
