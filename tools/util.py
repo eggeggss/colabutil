86a351bf3d48c0523364d06023494da6bb5e98df
eggeggss <eggeggss@gmail.com>
2019-04-28 19:38:52 +0800

    123

--- tools/util.py
+++ tools/util.py
@@ -3,11 +3,23 @@ import os
 import zipfile
 
 
+
+
 def gzip(gpath):
   local_zip = gpath
   zip_ref = zipfile.ZipFile(local_zip, 'r')
   zip_ref.extractall('.')
   zip_ref.close()
+
+def draw_history(grad_historys,type):
+  if 'acc' in type:
+     plt.plot(grad_historys.history['acc'])
+     plt.plot(grad_historys.history['val_acc'])
+     plt.legend(['acc','val_acc'],loc='upper left')
+  elif 'loss' in type:
+     plt.plot(grad_historys.history['loss'])
+     plt.plot(grad_historys.history['val_loss'])
+     plt.legend(['loss','val_loss'],loc='upper left')
 
 
 def show_train_history(train_history,train,validation):
